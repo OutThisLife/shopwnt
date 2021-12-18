8fb0d08e564c9228d292117edca4e60e7cca42fc
Talasan Nicholson <talasan.nicholson@gmail.com>
2021-12-18 15:06:54 -0600

    chore: uptick

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,3 @@
-import * as React from 'react'
-import { SWRConfig } from 'swr'
-import type { Product } from '~/../types'
-import { fetcher } from '~/lib'
 import { Home } from '~/templates'
 
-export default function Index({ fallback }: { fallback: Product[] }) {
-  return (
-    <SWRConfig value={{ fallback }}>
-      <Home />
-    </SWRConfig>
-  )
-}
-
-export async function getStaticProps() {
-  const data = await fetcher(
-    `https://fillyboo.myshopify.com/products.json?limit=15`
-  )
-
-  return { props: { fallback: [{ ...data, vendor: 'loveshackfancy' }] } }
-}
+export default Home
